99bfb3fc76b7515ad074cd9b5c809c66d2c1fad0
Ayoub1445 <ayoubannan16@gmail.com>
2025-01-16 23:52:13 +0100

    +edit icone

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(methodOverride('_method',{methods: ['POST','GET']}));
 app.set("view engine","ejs") 
 app.use(express.urlencoded({extended:true}))
 app.use('/',router)
+app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/ToDo' );
 
 
